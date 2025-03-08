f316689d3721af8568e0bd87b59afdeadbc662bb
mehedi <mehedisun@outlook.com>
2025-03-08 15:39:48 +0600

    updated docker file

--- gplay-auth-services/publish.bat
+++ gplay-auth-services/publish.bat
@@ -1,2 +1,2 @@
-dotnet publish ./src/gamex.Auth.Services/gamex.Auth.Services.csproj -c Release -o ./app
-robocopy C:\PROJECTS\gamex\gamex-auth-services\src\gamex.Auth.Services\StaticContents C:\PROJECTS\gamex\gamex.Auth.Services\app\StaticContents /E
+dotnet publish ./src/gamex.Auth.Services/gamex.Auth.Services.csproj -c Release -o ./app-output
+robocopy C:\PROJECTS\gamex\gamex-auth-services\src\gamex.Auth.Services\StaticContents C:\PROJECTS\gamex\gamex.Auth.Services\app-output\StaticContents /E
--- gplay-auth-services/publish.sh
+++ gplay-auth-services/publish.sh
@@ -1,2 +1,2 @@
-dotnet publish ./src/gamex.Auth.Services/gamex.Auth.Services.csproj -c Release -o ./app
-cp -r /Users/mehedimayall/PROJECTS/GM/gamex/gamex-auth-services/src/gamex.Auth.Services/StaticContents /Users/mehedimayall/PROJECTS/GM/gamex/gamex-auth-services/app/
+dotnet publish ./src/gamex.Auth.Services/gamex.Auth.Services.csproj -c Release -o ./app-output
+cp -r /Users/mehedimayall/PROJECTS/GM/gamex/gamex-auth-services/src/gamex.Auth.Services/StaticContents /Users/mehedimayall/PROJECTS/GM/gamex/gamex-auth-services/app-output/
